328cb66b8d3250683b605182061fc1596ea43ab0
ujumqin <sinegov@gmail.com>
2017-12-24 18:20:14 -0800

    Uploading nearly final scripts.

--- Final Project Files/Analyzer/ow_twitter_analyzer.R
+++ Final Project Files/Analyzer/ow_twitter_analyzer.R
@@ -11,68 +11,64 @@ library(e1071)
 
 #read all three files into R. Ensure they are not factors
 twitter <- read.csv("OWT_12-22.csv", stringsAsFactors = FALSE)
-
-#=================Solution to problem...hopefully=================
-#XXFirst create a duplicate column for the text and call it content_text
-#XXTokenize the text column
-#XXGroup by the text column. this will allow us to create a sentiment score for the text comment, without destroying the text in tokenization
-#XXDetermine the score for the text. This will be useful in machine learning later
-#Remove stopwords, punctuation. Change to lowercase and stem the words
-#Start the analytics!
+str(twitter)
 
 #duplicating the text column so we have a copy of the text
-owforums$text_topic <- owforums$text
+twitter$text_topic <- twitter$text
 
 #tokenize the text column
-tidy_owforums <- owforums %>%
-  group_by(X.) %>%
+tidy_twitter <- twitter %>%
+  group_by(twitter$text_topic) %>%
   mutate(linenumber = row_number()) %>%
   unnest_tokens(word, text) %>%
   ungroup()
 
 #get the sentiment for the tokenized words
-owforums_sentimentbing <- tidy_owforums %>%
+twitter_sentimentbing <- tidy_twitter %>%
   inner_join(get_sentiments("bing"))
 
 #create new columns to use in for loop calculations
-owforums_sentimentbing$score <- 0
-owforums_sentimentbing$count <- 1
+twitter_sentimentbing$score <- 0
+twitter_sentimentbing$count <- 1
 
 #this for loops codes the sentiment from bing into a -1 or 1. 1 is positive and -1 is negative. Also counts the number of sentiment words to be used later.
-for(i in 1:length(owforums_sentimentbing$sentiment)) {
-  if(owforums_sentimentbing$sentiment[i] == "negative") {
+for(i in 1:length(twitter_sentimentbing$sentiment)) {
+  if(twitter_sentimentbing$sentiment[i] == "negative") {
     print("negative")
-    owforums_sentimentbing$score[i] = -1
+    twitter_sentimentbing$score[i] = -1
   } else {
     print("positive")
-    owforums_sentimentbing$score[i] = 1
+    twitter_sentimentbing$score[i] = 1
   }
 }
 
 #this shows us what sentiment is for each post and groups it by post
-owforums_sentimentbing <- owforums_sentimentbing %>%
-  group_by(X.) %>%
+twitter_sentimentbing <- twitter_sentimentbing %>%
+  group_by(text_topic) %>%
   mutate(sentimentscore = sum(score)/sum(count))
 
 #identify the negative values
-owforums_sentimentbing$negative <- as.factor(owforums_sentimentbing$sentimentscore < 0)
-table(owforums_sentimentbing$negative)
-
-#create a new df without all of the unnecessary data
-owsentiment <- data.frame(owforums_sentimentbing$text_topic, owforums_sentimentbing$sentimentscore, owforums_sentimentbing$negative)
-names(owsentiment) <- c("forum_text","sentiment_score","negative")
+twitter_sentimentbing$negative <- as.factor(twitter_sentimentbing$sentimentscore < 0)
+table(twitter_sentimentbing$negative)
 
 #collapse the duplicated columns by text_topic
-owsentiment <- owsentiment[!duplicated(owsentiment$forum_text),]
+twitter_sentimentbing <- twitter_sentimentbing[!duplicated(twitter_sentimentbing$text_topic),]
+
+str(twitter_sentimentbing)
+
+#create a new df without all of the unnecessary data
+twittersentiment <- data.frame(twitter_sentimentbing$text_topic, twitter_sentimentbing$sentimentscore, twitter_sentimentbing$negative)
+names(twittersentiment) <- c("forum_text","sentiment_score","negative")
+
 
 #test code
-write.csv(owsentiment, file="duplicatesgone.csv")
+write.csv(twittersentiment, file="twitterduplicatesgone.csv")
 
 
 #Create a corpus and remove unnecessary words/text
 #Corpus is necessary to do predictive analytics
-owcorpus <- Corpus(VectorSource(owsentiment$forum_text))
-inspect(owcorpus[[100]])
+owcorpus <- Corpus(VectorSource(twittersentiment$forum_text))
+inspect(owcorpus[[10]])
 owcorpus <- tm_map(owcorpus, removePunctuation)
 owcorpus <- tm_map(owcorpus, tolower)
 owcorpus <- tm_map(owcorpus, removeWords, c("mercy", "bastion", stopwords("english")))
@@ -103,7 +99,7 @@ colnames(owpostSparses) = make.names(colnames(owpostSparses))
 
 write.csv(owpostSparses, file="owpostspare.csv")
 #add dependent variable
-owpostSparses$negative <- owsentiment$negative
+owpostSparses$negative <- twittersentiment$negative
 
 #setting a seed to get consistent results when running multiple times
 set.seed(1113)
@@ -124,30 +120,21 @@ cartGrid <- expand.grid(.cp=(1:50)*0.00001)
 train(negative ~., data=trainow, method="rpart", trControl=fitOWControl, tuneGrid=cartGrid)
 
 #second model created using recommended cp value
-forumCART2 <- rpart(negative ~., data=trainow, method="class",control=rpart.control(cp=0.0025))
+forumCART2 <- rpart(negative ~., data=trainow, method="class",control=rpart.control(cp=0.00038))
 prp(forumCART2, fallen.leaves = FALSE, tweak = 1.0, compress = TRUE, ycompress = TRUE, box.palette = "auto")
 
-
+#!!!!!!!!!!!!Tree model.WARNING! TAKES A LONG TIME TO RUN!!!!!!
+forumRF <- randomForest(negative ~., data = trainow)
 
 #testing model against test data. table is outputted for each model
-predictForumCart2 <- predict(forumCART2, newdata = testow, type = "class")
-table(testow$negative, predictForumCart2)
-(339+85)/(339+45+130+85)
-
-
 predictForumCart <- predict(forumCART, newdata=testow, type="class")
 table(testow$negative, predictForumCart)
-(364+48)/(364+23+167+48)
+(1063+109)/(1063+109+34+370)
 
-#takes a long time to run!
-forumRF <- randomForest(negative ~., data = trainow)
+predictForumCart2 <- predict(forumCART2, newdata = testow, type = "class")
+table(testow$negative, predictForumCart2)
+(1057+143)/(1057+143+40+336)
+
 predictRF <- predict(forumRF, newdata = testow)
-
 table(testow$negative, predictRF)
-
-
-
-
-
-
-
+(989+324)/(989+324+108+155)
