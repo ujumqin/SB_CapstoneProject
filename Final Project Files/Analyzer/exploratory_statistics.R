77fb0b69ee6145b77e277f674e2dff0c287f6051
ujumqin <sinegov@gmail.com>
2018-01-06 23:24:35 -0800

    Updated report for statistics. Added a lot more detail and revised the plots. Will need to fix the word cloud plots and look into creating a re-color of sentiment words.

--- Final Project Files/Analyzer/exploratory_statistics.R
+++ Final Project Files/Analyzer/exploratory_statistics.R
@@ -12,31 +12,48 @@ twitter <- read.csv(text=getURL("https://raw.githubusercontent.com/ujumqin/SB_Ca
 #twitter <- read.csv("OWT_12-22.csv", stringsAsFactors = FALSE)
 forums <- read.csv(text=getURL("https://raw.githubusercontent.com/ujumqin/SB_CapstoneProject/master/Final%20Project%20Files/OW%20Data/Raw%20Files/OWFORUMS12_22_FINAL.csv"), stringsAsFactors = FALSE)
 #forums <- read.csv("OWFORUMS12_22_FINAL.csv", stringsAsFactors = FALSE)
+all <- read.csv(text=getURL("https://raw.githubusercontent.com/ujumqin/SB_CapstoneProject/master/Final%20Project%20Files/OW%20Data/Raw%20Files/all.csv"), stringsAsFactors = FALSE)
+withouttwitter <- read.csv(text=getURL("https://raw.githubusercontent.com/ujumqin/SB_CapstoneProject/master/Final%20Project%20Files/OW%20Data/Raw%20Files/WithoutTwitter_FINAL.csv"), stringsAsFactors = FALSE)
+
 
 #duplicating the text column so we have a copy of the text. this text is used to group
 reddit$text_topic <- reddit$X.text.
 twitter$text_topic <- twitter$text
 forums$text_topic <- forums$text
+all$text_topic <- all$text
+withouttwitter$text_topic <- withouttwitter$text
 
 #-------------------------Tokenize Text-------------------------
 #tokenize the text column for reddit
 tidy_reddit <- reddit %>%
   group_by(text_topic) %>%
-  mutate(linenumber = row_number()) %>%
+  dplyr::mutate(linenumber = row_number()) %>%
   unnest_tokens(word, X.text.) %>%
   ungroup()
 
 #tokenize the text column for twitter
 tidy_twitter <- twitter %>%
   group_by(text_topic) %>%
-  mutate(linenumber = row_number()) %>%
+  dplyr::mutate(linenumber = row_number()) %>%
   unnest_tokens(word, text) %>%
   ungroup()
 
 #tokenize the text column for forums
 tidy_forums <- forums %>%
   group_by(X.) %>%
-  mutate(linenumber = row_number()) %>%
+  dplyr::mutate(linenumber = row_number()) %>%
+  unnest_tokens(word, text) %>%
+  ungroup()
+
+tidy_all <- all %>%
+  group_by(X.) %>%
+  dplyr::mutate(linenumber = row_number()) %>%
+  unnest_tokens(word, text) %>%
+  ungroup()
+
+tidy_notwitter <- withouttwitter %>%
+  group_by(X.) %>%
+  dplyr::mutate(linenumber = row_number()) %>%
   unnest_tokens(word, text) %>%
   ungroup()
 
@@ -55,6 +72,17 @@ forum_bing <- tidy_forums %>%
   inner_join(get_sentiments("bing")) %>%
   count(word, sentiment, sort = TRUE) %>%
   ungroup()
+
+all_bing <- tidy_all %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(word, sentiment, sort = TRUE) %>%
+  ungroup()
+
+withouttwitter_bing <- tidy_notwitter %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(word, sentiment, sort = TRUE) %>%
+  ungroup()
+
 
 #-------------------------Plotting Data (Bing)-------------------------
 reddit_bing %>%
@@ -93,6 +121,29 @@ forum_bing %>%
        x = NULL) +
   coord_flip()
 
+all_bing %>%
+  group_by(sentiment) %>%
+  top_n(10) %>%
+  ungroup() %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n, fill = sentiment)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~sentiment, scales = "free_y") +
+  labs(y = "Most Used Sentiment Words By Overwatch Community",
+       x = NULL) +
+  coord_flip()
+
+withouttwitter_bing %>%
+  group_by(sentiment) %>%
+  top_n(10) %>%
+  ungroup() %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n, fill = sentiment)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~sentiment, scales = "free_y") +
+  labs(y = "Most Used Sentiment Words By Overwatch Community",
+       x = NULL) +
+  coord_flip()
 #-------------------------nrc Sentiment-------------------------
 reddit_nrc <- tidy_reddit %>%
   inner_join(get_sentiments("nrc"))
@@ -103,12 +154,29 @@ twitter_nrc <- tidy_twitter %>%
 forum_nrc <- tidy_forums %>%
   inner_join(get_sentiments("nrc"))
 
+all_nrc <- tidy_all %>%
+  inner_join(get_sentiments("nrc"))
+
+withouttwitter_nrc <- tidy_all %>%
+  inner_join(get_sentiments("nrc"))
+
 #-------------------------Plotting Data (nrc)-------------------------
-ggplot(reddit_nrc, aes(x=sentiment)) +
-  geom_bar(aes(y=..count.., fill =..count..)) +
-  theme(axis.text.x = element_text(size = 10, angle = 45, hjust = 1, vjust = 1)) +
-  ggtitle("reddit") +
-  coord_cartesian(ylim = c(0,11000), expand = TRUE)
+ggplot(reddit_nrc, aes(x="", fill=sentiment)) +
+  geom_bar(width = 1) +
+  ggtitle("reddit") 
+
+reddit_bar
+
+reddit_bar + coord_polar("y")
+
+ggplot(forum_nrc, aes(x="", fill=sentiment)) +
+  geom_bar(width = 1) +
+  ggtitle("Overwatch Forums") 
+
+forum_bar
+
+forum_bar + coord_polar("y")
+
 
 ggplot(twitter_nrc, aes(x=sentiment)) +
   geom_bar(aes(y=..count.., fill =..count..)) +
@@ -121,6 +189,12 @@ ggplot(forum_nrc, aes(x=sentiment)) +
   theme(axis.text.x = element_text(size = 10, angle = 45, hjust = 1, vjust = 1)) +
   ggtitle("Overwatch Forums") +
   coord_cartesian(ylim = c(0,11000), expand = TRUE)
+
+ggplot(withouttwitter_nrc, aes(x=sentiment)) +
+  geom_bar(aes(y=..count.., fill =..count..)) +
+  theme(axis.text.x = element_text(size = 10, angle = 45, hjust = 1, vjust = 1)) +
+  ggtitle("All Communities") +
+  coord_cartesian(ylim = c(0,24000), expand = TRUE)
   
 #-------------------------afinn Sentiment-------------------------
 reddit_afinn <- tidy_reddit %>%
@@ -132,9 +206,16 @@ twitter_afinn <- tidy_twitter %>%
 forum_afinn <- tidy_forums %>%
   inner_join(get_sentiments("afinn"))
 
+all_afinn <- tidy_all %>%
+  inner_join(get_sentiments("afinn"))
+
+withouttwitter_afinn <- tidy_notwitter %>%
+  inner_join(get_sentiments("afinn"))
+
 #-------------------------Plotting data (Afinn)-------------------------
+
 ggplot(twitter_afinn, aes(x=score)) +
-  geom_bar(aes(y=..count.., fill=..count..)) + 
+  geom_bar(aes(y=..count.., fill= ..count..)) + 
   labs(x="afinn score") +
   xlab("afinn score")+
   coord_cartesian(ylim = c(0,7000), expand = TRUE) +
@@ -157,6 +238,14 @@ ggplot(reddit_afinn, aes(x=score)) +
   ggtitle("Reddit") +
   scale_x_continuous(breaks = -5:5) 
 
+ggplot(withouttwitter_afinn, aes(x=score)) +
+  geom_bar(aes(y=..count.., fill=..count..)) + 
+  labs(x="afinn score") +
+  xlab("afinn score")  +
+  coord_cartesian(ylim = c(0,13000), expand = TRUE)+
+  ggtitle("All") +
+  scale_x_continuous(breaks = -5:5) 
+
 #-------------------------Word Clouds-------------------------
 
 tidy_forums %>%
@@ -174,7 +263,19 @@ tidy_twitter %>%
   count(word) %>%
   with(wordcloud(word, n, max.words = 50))
 
+tidy_all %>%
+  anti_join(stop_words) %>%
+  count(word) %>%
+  with(wordcloud(word, n, max.words = 50))
+
+tidy_notwitter %>%
+  anti_join(stop_words) %>%
+  count(word) %>%
+  with(wordcloud(word, n, max.words = 50))
+
 #-------------------------Word Clouds (negative vs positive)-------------------------
+par(mfrow=c(2,2))
+
 tidy_forums %>%
   inner_join(get_sentiments("bing")) %>%
   count(word, sentiment, sort = TRUE) %>%
@@ -190,6 +291,13 @@ tidy_forums %>%
                    max.words = 80)
 
 tidy_twitter %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(word, sentiment, sort = TRUE) %>%
+  acast(word ~ sentiment, value.var = "n", fill = 0) %>%
+  comparison.cloud(colors = c("gray20", "gray80"),
+                   max.words = 80)
+
+tidy_notwitter %>%
   inner_join(get_sentiments("bing")) %>%
   count(word, sentiment, sort = TRUE) %>%
   acast(word ~ sentiment, value.var = "n", fill = 0) %>%
