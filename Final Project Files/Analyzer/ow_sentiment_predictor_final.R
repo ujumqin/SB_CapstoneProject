6389e5d1f06485212cc373b42785de4dbf08afa1
ujumqin <sinegov@gmail.com>
2018-01-14 22:02:15 -0800

    Code for creating auto-sentiment analyzer

--- Final Project Files/Analyzer/ow_sentiment_predictor_final.R
+++ Final Project Files/Analyzer/ow_sentiment_predictor_final.R
@@ -3,6 +3,9 @@ library(tidytext)
 library(tm)
 library(caTools)
 library(rpart.plot)
+library(e1071)
+library(caret)
+library(randomForest)
 
 #-------------------------Reading Files Into R-------------------------#
 #We are only interested in using reddit and forums data. Twitter was found to contain 
@@ -116,8 +119,11 @@ afinn_maker <- function(tidy_data) {
 
 #--------------------Perform afinn--------------------#
 forum_afinn <- afinn_maker(tidy_forums)
+forum_afinn$negative <- as.factor(forum_afinn$negative)
 reddit_afinn <- afinn_maker(tidy_reddit)
+reddit_afinn$negative <- as.factor(reddit_afinn$negative)
 forandred_afinn <- afinn_maker(tidy_forandred)
+forandred_afinn$negative <- as.factor(forandred_afinn$negative)
 
 #----------------PREP DATA FOR MACHINE LEARNING----------------#
 
@@ -173,25 +179,215 @@ a_forum_predict <- process_corpus(aforum_corpus, forum_afinn)
 a_reddit_predict <- process_corpus(areddit_corpus, reddit_afinn)
 a_forandred_predict <- process_corpus(aforandred_corpus, forandred_afinn)
 
-
-
-
-#----------------Test Code----------------#
+#----------------Splitting into Test and Train Sets----------------#
 #setting a seed to get consistent results when running multiple times
 set.seed(1113)
 
 #split the data into a training set and a test set. 
 
-
-split <- sample.split(b_forandred_predict$negative, SplitRatio = 0.8)
-trainow <- subset(b_forandred_predict, split == TRUE)
-testow <- subset(b_forandred_predict, split == FALSE)
-
-#First model created. Default values used.
-forumCART <- rpart(negative ~., data=trainow, method="class")
-#plot the model
-prp(forumCART)
+#set the SplitRatio into variable so we can modify easily for all data sets. This ratio will go into the test set (20% goes into test set)
+sr <- 0.2
 
 
-predictForumCart <- predict(forumCART, newdata=testow, type="class")
-table(testow$negative, predictForumCart)
+#Bing Forums
+bforum_split <- sample.split(b_forum_predict$negative, SplitRatio = sr)
+bforum_test <- subset(b_forum_predict, bforum_split == TRUE)
+bforum_train <- subset(b_forum_predict, bforum_split == FALSE)
+
+#Bing Reddit
+breddit_split <- sample.split(b_reddit_predict$negative, SplitRatio = sr)
+breddit_test <- subset(b_reddit_predict, breddit_split == TRUE)
+breddit_train <- subset(b_reddit_predict, breddit_split == FALSE)
+
+#Bing both
+bforandred_split <- sample.split(b_forandred_predict$negative, SplitRatio = sr)
+bforandred_test <- subset(b_forandred_predict, bforandred_split == TRUE)
+bforandred_train <- subset(b_forandred_predict, bforandred_split == FALSE)
+
+#Afinn Forums
+aforum_split <- sample.split(a_forum_predict$negative, SplitRatio = sr)
+aforum_test <- subset(a_forum_predict, aforum_split == TRUE)
+aforum_train <- subset(a_forum_predict, aforum_split == FALSE)
+
+#Afinn Reddit
+areddit_split <- sample.split(a_reddit_predict$negative, SplitRatio = sr)
+areddit_test <- subset(a_reddit_predict, areddit_split == TRUE)
+areddit_train <- subset(a_reddit_predict, areddit_split == FALSE)
+
+#Afinn Both
+aforandred_split <- sample.split(a_forandred_predict$negative, SplitRatio = sr)
+aforandred_test <- subset(a_forandred_predict, aforandred_split == TRUE)
+aforandred_train <- subset(a_forandred_predict, aforandred_split == FALSE)
+
+#----------------Model Functions----------------#
+#First model created. Automatic. No validation used. Default values used.
+CARTMODELMAKER <- function(traindata) {
+  cartmodel1 <- rpart(negative~., data = traindata, method = "class")
+  return(cartmodel1)
+}
+
+#Second Model using cross validation. 
+
+#First set folds and cross validation method. Then determine increments.
+fitOWControl = trainControl(method="cv",number=10)
+cartGrid <- expand.grid(.cp=(1:50)*0.00001)
+
+#function to find the best cp
+cpmaker <- function(traindata,fold,increment) {
+  fitOWControl = trainControl(method="cv",number=fold)
+  cartGrid <- expand.grid(.cp=(1:50)*increment)
+  cpvalue <- train(negative ~., data=traindata, method="rpart", trControl=fitOWControl, tuneGrid=cartGrid)
+  return(cpvalue)
+}
+
+#build the model using results from CARTMODELTWO
+CARTMODEL2 <- function(traindata, cp) {
+  cartmodel2train <-  rpart(negative ~., data=traindata, method="class",control=rpart.control(cp=cp))
+}
+
+#Third Model (Random Forest)
+RFMODEL <- function(trainingset) {
+  randomforestmodel <- randomForest(negative ~., data = trainingset)
+}
+
+
+#Function to test the model with test data
+CARTMODELTEST <- function(model,testdata) {
+  predictmodel <- predict(model, newdata=testdata, type="class")
+  return(predictmodel)
+}
+
+#Function to show the results
+tablechecker <- function(testdata, modelresults) {
+  tablecheck <- table(testdata$negative, modelresults)
+  confusionMatrix(tablecheck)
+}
+
+#----------------1st Model (Automatic), Testing----------------#
+
+#----------Bing Test----------#
+#Model using the training set
+bingforummodel1 <- CARTMODELMAKER(bforum_train)
+bingredditmodel1 <- CARTMODELMAKER(breddit_train)
+bingforandredmode1l <- CARTMODELMAKER(bforandred_train)
+prp(bingforummodel1)
+prp(bingredditmodel1)
+prp(bingforummodel1)
+
+#Test the model using test set
+bingforumtest1 <- CARTMODELTEST(bingforummodel1, bforum_test)
+bingreddittest1 <- CARTMODELTEST(bingredditmodel1, breddit_test)
+bingforandredtest1 <- CARTMODELTEST(bingforandredmode1l, bforandred_test)
+
+
+#----------Afinn Test----------#
+afinnforummodel1 <- CARTMODELMAKER(aforum_train)
+afinnredditmodel1 <- CARTMODELMAKER(areddit_train)
+afinnforandredmode1l <- CARTMODELMAKER(aforandred_train)
+prp(afinnforummodel1)
+prp(afinnredditmodel1)
+prp(afinnforandredmode1l)
+
+#Test the model using test set
+afinnforumtest1 <- CARTMODELTEST(afinnforummodel1, aforum_test)
+afinnreddittest1 <- CARTMODELTEST(afinnredditmodel1, areddit_test)
+afinnforandredtest1 <- CARTMODELTEST(afinnforandredmode1l, aforandred_test)
+
+#----------------2nd Model (Cross Validated), Testing----------------#
+
+#----------Bing Test----------#
+#Figure out the cp values. May take a minute to find values
+cpbforums <- cpmaker(bforum_train, 10, 0.002)
+cpbreddit <- cpmaker(breddit_train, 10, 0.002)
+cpbforandred <- cpmaker(bforandred_train, 10, 0.001)
+cpbforums
+cpbreddit
+cpbforandred
+
+#Create models with new cp values
+bingforummodel2 <- CARTMODEL2(bforum_train, 0.002)
+bingredditmodel2 <- CARTMODEL2(breddit_train, 0.002)
+bingforandredmodel2 <- CARTMODEL2(bforandred_train, 0.003)
+prp(bingforummodel2)
+prp(bingredditmodel2)
+prp(bingforandredmodel2)
+
+#Test the models 
+bingforumtest2 <- CARTMODELTEST(bingforummodel2, bforum_test)
+bingreddittest2 <- CARTMODELTEST(bingredditmodel2, breddit_test)
+bingforandredtest2 <- CARTMODELTEST(bingforandredmodel2, bforandred_test)
+
+
+#----------Afinn Test----------#
+#Figure out the cp values. May take a minute to find values
+cpaforums <- cpmaker(aforum_train, 10, 0.0001)
+cpareddit <- cpmaker(areddit_train, 10, 0.0001)
+cpaforandred <- cpmaker(aforandred_train, 10, 0.0001)
+cpaforums
+cpareddit
+cpaforandred
+
+#Create models with new cp values
+afinnforummodel2 <- CARTMODEL2(aforum_train, 0.0028)
+afinnredditmodel2 <- CARTMODEL2(areddit_train, 0.0041)
+afinnforandredmodel2 <- CARTMODEL2(aforandred_train, 0.0012)
+prp(afinnforummodel2)
+prp(afinnredditmodel2)
+prp(afinnforandredmodel2)
+
+#Test the models 
+afinnforumtest2 <- CARTMODELTEST(afinnforummodel2, aforum_test)
+afinnreddittest2 <- CARTMODELTEST(afinnredditmodel2, areddit_test)
+afinnforandredtest2 <- CARTMODELTEST(afinnforandredmodel2, aforandred_test)
+
+#----------------3rd Model (Random Forest), Testing----------------#
+#!!!!!!!!!!!!WARNING! TAKES A LONG TIME TO RUN!!!!!!
+
+bingforummodel3 <- RFMODEL(bforum_train)
+bingredditmodel3 <- RFMODEL(breddit_train)
+bingforandredmodel3 <- RFMODEL(bforandred_train)
+
+afinnforummodel3 <- RFMODEL(aforum_train)
+afinnredditmodel3 <- RFMODEL(areddit_train)
+afinnforandredmodel3 <- RFMODEL(aforandred_train)
+
+#Test the models 
+bingforumtest3 <- CARTMODELTEST(bingforummodel3, bforum_test)
+bingreddittest3 <- CARTMODELTEST(bingredditmodel3, breddit_test)
+bingforandredtest3 <- CARTMODELTEST(bingforandredmodel3, bforandred_test)
+afinnforumtest3 <- CARTMODELTEST(afinnforummodel3, aforum_test)
+afinnreddittest3 <- CARTMODELTEST(afinnredditmodel3, areddit_test)
+afinnforandredtest3 <- CARTMODELTEST(afinnforandredmodel3, aforandred_test)
+
+
+#-------------------------Compare Models-------------------------#
+
+#Forum Models (bing)
+tablechecker(bforum_test, bingforumtest1)
+tablechecker(bforum_test, bingforumtest2)
+tablechecker(bforum_test, bingforumtest3)
+
+#Reddit Models (bing)
+tablechecker(breddit_test, bingreddittest1)
+tablechecker(breddit_test, bingreddittest2)
+tablechecker(breddit_test, bingreddittest3)
+
+#Combined Forum & Reddit Models (bing)
+tablechecker(bforandred_test, bingforandredtest1)
+tablechecker(bforandred_test, bingforandredtest2)
+tablechecker(bforandred_test, bingforandredtest3)
+
+#Forum Models (afinn)
+tablechecker(aforum_test, afinnforumtest1)
+tablechecker(aforum_test, afinnforumtest2)
+tablechecker(aforum_test, afinnforumtest3)
+
+#Reddit Models (afinn)
+tablechecker(areddit_test, afinnreddittest1)
+tablechecker(areddit_test, afinnreddittest2)
+tablechecker(areddit_test, afinnreddittest3)
+
+#Combined Forum & Reddit Models (afinn)
+tablechecker(aforandred_test, afinnforandredtest1)
+tablechecker(aforandred_test, afinnforandredtest2)
+tablechecker(aforandred_test, afinnforandredtest3)
